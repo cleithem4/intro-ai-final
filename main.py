d850b4201ac671861a47e6710edb3999292155b3
cleithem <cleithem@iu.edu>
2024-12-08 18:03:22 -0500

    d

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     from random import seed
 
     # Optional: Seed random number generator for reproducible results
-    seed(3)
+    seed(3532535)
 
     # Initialize players
-    player1 = PlayerMM(max_depth=3)  # AI using Alpha-Beta Pruning
+    player1 = PlayerMM(max_depth=4)  # AI using Alpha-Beta Pruning
     player2 = PlayerAB(max_depth=3)  # AI using MiniMax
 
     # Create and start the game
